27167e4363b545c87f711fb31bf1c4103d8190a6
Jordana R Barbosa <jordananabarbosa@hotmail.com>
2025-03-24 18:04:41 -0300

    inserindo frases

--- palavra_viva/lib/main.dart
+++ palavra_viva/lib/main.dart
@@ -30,10 +30,31 @@ class MyHomePage extends StatefulWidget {
 
 class MyHomePageState extends State<MyHomePage> {
   int _numeroAleatorio = 0;
+final List<String> _frases = [
+  "No princípio, Deus criou os céus e a terra. - Gênesis 1:1",
+  "O Senhor é meu pastor, nada me faltará. - Salmo 23:1",
+  "Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna. - João 3:16",
+  "Tudo posso naquele que me fortalece. - Filipenses 4:13",
+  "Entrega o teu caminho ao Senhor; confia nele, e ele tudo fará. - Salmo 37:5",
+  "Confia no Senhor de todo o teu coração e não te estribes no teu próprio entendimento. - Provérbios 3:5",
+  "Clama a mim, e responder-te-ei e anunciar-te-ei coisas grandes e firmes, que não sabes. - Jeremias 33:3",
+  "Não to mandei eu? Esforça-te, e tem bom ânimo; não temas, nem te espantes; porque o Senhor teu Deus é contigo, por onde quer que andares. - Josué 1:9",
+  "E sabemos que todas as coisas contribuem juntamente para o bem daqueles que amam a Deus, daqueles que são chamados por seu decreto. - Romanos 8:28",
+  "O choro pode durar uma noite, mas a alegria vem pela manhã. - Salmo 30:5",
+  "Deixo-vos a paz, a minha paz vos dou; não vo-la dou como o mundo a dá. - João 14:27",
+  "Buscai primeiro o Reino de Deus, e a sua justiça, e todas estas coisas vos serão acrescentadas. - Mateus 6:33",
+  "O Senhor dos Exércitos está conosco; o Deus de Jacó é o nosso refúgio. - Salmo 46:7",
+  "Alegrai-vos sempre no Senhor; outra vez digo: alegrai-vos. - Filipenses 4:4",
+  "Mas os que esperam no Senhor renovarão as forças, subirão com asas como águias, correrão, e não se cansarão; andarão, e não se fatigarão. - Isaías 40:31",
+  "O Senhor te guardará de todo mal; ele guardará a tua alma. - Salmo 121:7",
+  "Aquele que habita no esconderijo do Altíssimo, à sombra do Onipotente descansará. - Salmo 91:1",
+  "Se Deus é por nós, quem será contra nós? - Romanos 8:31",
+  "Em tudo dai graças, porque esta é a vontade de Deus em Cristo Jesus para convosco. - 1 Tessalonicenses 5:18"
+];
 
   void _incrementCounter() {
     setState(() {
-      _numeroAleatorio = Random().nextInt(10);  // Gera um número aleatório de 0 a 9
+      _numeroAleatorio = Random().nextInt(19);  
     });
   }
 
@@ -49,11 +70,11 @@ class MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Text(
               'Quantidade de palavras que transformam:',
-              style: GoogleFonts.caveat(fontSize: 24),  // Usando a fonte Caveat com tamanho 24
+              style: GoogleFonts.caveat(fontSize: 24),  
             ),
             Text(
-              '$_numeroAleatorio',
-              style: GoogleFonts.caveat(fontSize: 48, fontWeight: FontWeight.bold),  // Usando a fonte Caveat em negrito e maior
+              _frases[_numeroAleatorio],
+              style: GoogleFonts.caveat(fontSize: 48, fontWeight: FontWeight.bold),  
             ),
           ],
         ),
@@ -61,7 +82,7 @@ class MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Incrementar',
-        child: const Icon(Icons.add),  // Adicionada a palavra "const" para otimizar
+        child: const Icon(Icons.add),  
       ),
     );
   }
